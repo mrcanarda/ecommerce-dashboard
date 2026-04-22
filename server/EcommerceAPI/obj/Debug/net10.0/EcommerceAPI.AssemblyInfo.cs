c097ba1d9e8ebc26b05673a5ca7d1f0159e60fde
Can Arda <canarda.dev@gmail.com>
2026-04-22 17:38:00 +0200

    fix: add Dockerfile for Railway deployment

--- server/EcommerceAPI/obj/Debug/net10.0/EcommerceAPI.AssemblyInfo.cs
+++ server/EcommerceAPI/obj/Debug/net10.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18dafc7f3884ec08461c7db1bcbe6f8383b1f2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e3334863604fbfe0a0ae004d3c35085008c473")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
