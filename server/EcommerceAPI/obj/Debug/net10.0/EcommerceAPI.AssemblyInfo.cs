3fa5ebad90e19e448d89ed67e4adf286a2c0379e
Can Arda <canarda.dev@gmail.com>
2026-04-22 19:03:59 +0200

    fix: remove sln file to fix Railway build

--- server/EcommerceAPI/obj/Debug/net10.0/EcommerceAPI.AssemblyInfo.cs
+++ server/EcommerceAPI/obj/Debug/net10.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e3334863604fbfe0a0ae004d3c35085008c473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4703bb7177f5e78c339a78eb99d583ac4ee8785")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
